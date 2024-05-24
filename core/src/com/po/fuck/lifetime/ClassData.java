b4aca99562ec3587bb99090288e74f0f4cb16ed5
Eduard Tykhoniuk <edik.tikhoniuk@gmail.com>
2024-05-25 00:26:02 +0200

    Add javadoc and null check to the `ClassData`

--- core/src/com/po/fuck/lifetime/ClassData.java
+++ core/src/com/po/fuck/lifetime/ClassData.java
@@ -7,8 +7,18 @@ public final class ClassData<T> {
     public Consumer<T> constructor;
     public Consumer<T> destructor;
 
-    // cls should not be null, but constructor and destructor could be
+    /**
+     * Constructs a class data for a `Manager`
+     *
+     * @param cls         class for which this data is applied to, should not be
+     *                    null
+     * @param constructor can be null
+     * @param destructor  can be null
+     */
     public ClassData(Class<T> cls, Consumer<T> constructor, Consumer<T> destructor) {
+        if (this.cls == null)
+            throw new NullPointerException("Cls can not be null");
+
         this.cls = cls;
         this.constructor = constructor;
         this.destructor = destructor;
