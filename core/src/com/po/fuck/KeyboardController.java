7857a6641290c4610cb22c982ee2d65b0609fd60
Eduard Tykhoniuk <edik.tikhoniuk@gmail.com>
2024-05-27 02:40:42 +0200

    Fix when key was pressed before the death

--- core/src/com/po/fuck/KeyboardController.java
+++ core/src/com/po/fuck/KeyboardController.java
@@ -6,27 +6,43 @@ import com.badlogic.gdx.math.Vector2;
 import com.po.fuck.movement.Boost;
 
 public class KeyboardController extends InputAdapter {
-    private final Vector2 direction = new Vector2(0, 0);
     private final Player player;
+
+    // Prevent the situation when the buttons were pressed before the game and did
+    // not get register.
+    private boolean W = false, A = false, S = false, D = false;
 
     KeyboardController(Player player) {
         this.player = player;
+    }
+
+    private Vector2 getDirection() {
+        Vector2 result = new Vector2();
+        if (W)
+            result.y -= 1;
+        if (A)
+            result.x -= 1;
+        if (S)
+            result.y += 1;
+        if (D)
+            result.x += 1;
+        return result;
     }
 
     @Override
     public boolean keyDown(int keycode) {
         switch (keycode) {
             case Input.Keys.W:
-                direction.y -= 1;
+                W = true;
                 break;
             case Input.Keys.A:
-                direction.x -= 1;
+                A = true;
                 break;
             case Input.Keys.S:
-                direction.y += 1;
+                S = true;
                 break;
             case Input.Keys.D:
-                direction.x += 1;
+                D = true;
                 break;
             case Input.Keys.CONTROL_LEFT:
                 if (player.movement.get() instanceof Boost)
@@ -34,7 +50,7 @@ public class KeyboardController extends InputAdapter {
                 break;
         }
 
-        player.movement.get().setDirection(direction);
+        player.movement.get().setDirection(getDirection());
         return true;
     }
 
@@ -42,20 +58,20 @@ public class KeyboardController extends InputAdapter {
     public boolean keyUp(int keycode) {
         switch (keycode) {
             case Input.Keys.W:
-                direction.y += 1;
+                W = false;
                 break;
             case Input.Keys.A:
-                direction.x += 1;
+                A = false;
                 break;
             case Input.Keys.S:
-                direction.y -= 1;
+                S = false;
                 break;
             case Input.Keys.D:
-                direction.x -= 1;
+                D = false;
                 break;
         }
 
-        player.movement.get().setDirection(direction);
+        player.movement.get().setDirection(getDirection());
         return true;
     }
 }
