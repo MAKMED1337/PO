6f61770d51500f2c5c97fd172fcc5c35e56598b6
Ryhor Koshman <ryhor.koshman@gmail.com>
2024-05-28 02:05:51 +0200

    Constants renamed.

--- core/src/com/po/fuck/Coins.java
+++ core/src/com/po/fuck/Coins.java
@@ -8,12 +8,12 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Vector2;
 import com.po.fuck.updates.Drawable;
 
-import static com.po.fuck.Constants.COIN_NUMBER_POS_X;
-import static com.po.fuck.Constants.COIN_NUMBER_POS_Y;
+import static com.po.fuck.Constants.COIN_COUNTER_POSITION_X;
+import static com.po.fuck.Constants.COIN_COUNTER_POSITION_Y;
 import static com.po.fuck.Constants.SPRITE_COIN_POSITION_Y;
 import static com.po.fuck.Constants.SPRITE_COIN_POSITION_X;
-import static com.po.fuck.Constants.WIDTH_OF_COIN_NUMBER;
-import static com.po.fuck.Constants.TEXT_SCALE;
+import static com.po.fuck.Constants.WIDTH_OF_COIN_COUNTER_DIGIT;
+import static com.po.fuck.Constants.COINS_COUNTER_TEXT_SCALE;
 
 
 public class Coins implements Drawable {
@@ -28,10 +28,10 @@ public class Coins implements Drawable {
     public void draw(CenterDrawer drawer) {
         BitmapFont font = new BitmapFont();
         font.setColor(Color.YELLOW);
-        font.getData().scale(TEXT_SCALE);
+        font.getData().scale(COINS_COUNTER_TEXT_SCALE);
         String text = Integer.toString(coins);
-        drawer.drawText(text, font, new Vector2(COIN_NUMBER_POS_X, COIN_NUMBER_POS_Y));
+        drawer.drawText(text, font, new Vector2(COIN_COUNTER_POSITION_X, COIN_COUNTER_POSITION_Y));
         float len = text.length();
-        drawer.draw(sprite, new Vector2(SPRITE_COIN_POSITION_X + WIDTH_OF_COIN_NUMBER * len, SPRITE_COIN_POSITION_Y));
+        drawer.draw(sprite, new Vector2(SPRITE_COIN_POSITION_X + WIDTH_OF_COIN_COUNTER_DIGIT * len, SPRITE_COIN_POSITION_Y));
     }
 }
--- core/src/com/po/fuck/Constants.java
+++ core/src/com/po/fuck/Constants.java
@@ -18,13 +18,16 @@ public final class Constants {
 
     /* health bar */
     public static float HEALTHBAR_OFFSET = 5;
+
+    /* team tags */
     public static final int ENEMY_TEAM_TAG = 0;
     public static final int PLAYER_TEAM_TAG = 1;
 
+    /* coins */
     public static final float SPRITE_COIN_POSITION_X = 80;
     public static final float SPRITE_COIN_POSITION_Y = 45f;
-    public static final float COIN_NUMBER_POS_X = 35;
-    public static final float WIDTH_OF_COIN_NUMBER = 17;
-    public static final float COIN_NUMBER_POS_Y = GAME_HEIGHT - 30;
-    public static final float TEXT_SCALE = 1.5f;
+    public static final float COIN_COUNTER_POSITION_X = 35;
+    public static final float COIN_COUNTER_POSITION_Y = GAME_HEIGHT - 30;
+    public static final float WIDTH_OF_COIN_COUNTER_DIGIT = 17;
+    public static final float COINS_COUNTER_TEXT_SCALE = 1.5f;
 }
