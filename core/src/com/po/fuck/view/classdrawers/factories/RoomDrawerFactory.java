99bfb8d5fdc406adcd00a9d1e482f5cb0ae1bd77
Andrii Stolitnii <andrii.stolitnii01@gmail.com>
2024-06-02 14:09:30 +0200

    Minor fix

--- core/src/com/po/fuck/view/classdrawers/factories/RoomDrawerFactory.java
+++ core/src/com/po/fuck/view/classdrawers/factories/RoomDrawerFactory.java
@@ -1,11 +1,11 @@
 package com.po.fuck.view.classdrawers.factories;
 
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.po.fuck.view.CenterDrawer;
 import com.po.fuck.view.Renderer;
+import com.po.fuck.view.classdrawers.Misc;
 import com.po.fuck.view.classdrawers.ObjectDrawer;
 import com.po.fuck.model.Room;
 
@@ -13,7 +13,7 @@ public class RoomDrawerFactory {
     
     static {
         Renderer.addDrawer(Room.class, 
-        RoomDrawerFactory.get(Room.class, new Animation<TextureRegion>(1f,new Sprite(new Texture("island2.png")))));
+        RoomDrawerFactory.get(Room.class, Misc.getAnimation("island2.png")));
     }
 
     public static <T extends Room> ObjectDrawer<T> get(Class<T> clz, Animation<TextureRegion> animation){
